64552455e690ee4ca1008037026b3412da70da16
Mazlum <mazlumsert@gmail.com>
2017-03-31 09:52:06 +0200

    Mazlum

--- src/hashmap/Map.java
+++ src/hashmap/Map.java
@@ -11,11 +11,11 @@ package hashmap;
  */
 public interface Map<K, V>
 {
-   public V put(K key, V value);
-   public V get(K key);
-   public V remove(K key);
-   public int size();
-   public default boolean isEmpty()
+   V put(K key, V value);
+   V get(K key);
+   V remove(K key);
+   int size();
+   default boolean isEmpty()
    {
        return size() == 0;
    }
